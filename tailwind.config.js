d5e70d46992b64681d242bef81c6a072a71faefa
fsakuya <futamori.1991@gmail.com>
2023-06-05 22:23:11 +0900

    ユーザー・オーナー・管理者用登録画面を作成

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,24 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
+  important: true,
+  content: [
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './storage/framework/views/*.php',
+    './resources/views/**/*.blade.php',
+  ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-            },
-        },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        customBlue: '#305DFF',
+      },
     },
+  },
 
-    plugins: [require('@tailwindcss/forms')],
+
+  plugins: [require('@tailwindcss/forms')],
 };
