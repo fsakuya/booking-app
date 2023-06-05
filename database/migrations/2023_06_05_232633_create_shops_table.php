20a71579a9dd6bd6d0576697f3131566ea62f412
fsakuya <futamori.1991@gmail.com>
2023-06-05 23:43:32 +0900

    home画面（店舗一覧画面）のマイグレーション実施

---
+++ database/migrations/2023_06_05_232633_create_shops_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShopsTable extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('shops', function (Blueprint $table) {
+      $table->id();
+      $table->foreignId('owner_id')
+        ->constrained();
+      $table->foreignId('area_id')
+        ->constrained();
+      $table->foreignId('genre_id')
+        ->constrained();
+      $table->string('name');
+      $table->text('information');
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('shops');
+  }
+}
