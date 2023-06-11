2f42584dd77ea99fcd51d284dbdea02e8bd7da22
fsakuya <futamori.1991@gmail.com>
2023-06-11 21:56:13 +0900

    マイページ作成少し

--- routes/web.php
+++ routes/web.php
@@ -23,16 +23,23 @@ Route::get('/dashboard', function () {
   return view('user.dashboard');
 })->middleware(['auth:users'])->name('dashboard');
 
+Route::get('/mypage', function () {
+  if (auth()->check()) {
+    return view('user.menu-loggedin');
+  } else {
+    return view('user.menu-not-loggedin');
+  }
+});
 
 Route::get('/', [ShopListController::class, 'index'])->name('list.index');
 Route::get('/show/{id}', [ShopListController::class, 'show'])->name('list.show');
 Route::post('/search', [ShopListController::class, 'search'])->name('list.search');
 
-
-Route::middleware('auth:users')->group(function(){
+Route::middleware('auth:users')->group(function () {
   Route::post('/reserve/{id}', [ReserveController::class, 'store'])->name('reserve.store');
   Route::get('/reserve-done', function () {
-    return view('user.reserve-done');});
+    return view('user.reserve-done');
+  });
 });
 
 require __DIR__ . '/auth.php';
