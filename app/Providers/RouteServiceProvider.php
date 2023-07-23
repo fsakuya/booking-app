0e8c8002947674777b61fe997f2b05f465fc0a0e
fsakuya <futamori.1991@gmail.com>
2023-07-23 18:25:29 +0900

    一旦コミット

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
    * @var string
    */
   public const HOME = '/';
-  public const OWNER_HOME = '/owner/shops';
+  public const OWNER_HOME = '/owner';
   public const ADMIN_HOME = '/admin/dashboard';
 
   /**
