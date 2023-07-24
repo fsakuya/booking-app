4e86682c9d94e2dd8a409c2ca8799960948227cf
fsakuya <futamori.1991@gmail.com>
2023-07-24 23:20:25 +0900

    リマインダーメール修正

--- resources/views/owner/reservationReminder.blade.php
+++ resources/views/owner/reservationReminder.blade.php
@@ -1,7 +1,7 @@
-<p>ご予約頂きありがとうございます。</p><br>
+<p>ご予約頂きありがとうございます。</p>
 <p>予約日時が近づいて来ましたので、ご連絡致します。</p><br>
-<p>予約情報です。</p>
-<p>予約者名：{{ $userName }}</p>
+<p>予約情報は以下の通りです。</p>
+<p>予約者名：{{ $userName }}様</p>
 <p>予約店舗：{{ $reservationStore }}</p>
 <p>予約日：{{ $reservationDate }}</p>
 <p>予約時間：{{ $reservationTime }}</p>
